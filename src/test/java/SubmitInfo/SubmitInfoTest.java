1a1196116581d8c799bc074936e88b6628b98a48
SidimarKali <sidimarcoelho@msn.com>
2019-08-20 22:40:29 -0300

    Validação de execução correta

--- src/test/java/SubmitInfo/SubmitInfoTest.java
+++ src/test/java/SubmitInfo/SubmitInfoTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -36,6 +37,14 @@ public class SubmitInfoTest{
         driver.findElement(By.name("entry.1643251653")).sendKeys("Sidimar Garcia Coelho");
         driver.findElement(By.name("entry.17350902")).sendKeys("sidimar.coelho@msn.com");
         driver.findElement(By.name("entry.975579715")).sendKeys("Joinville SC");
+        driver.findElement(By.name("entry.2005929968")).sendKeys("Não foi possivel " +
+                "Estruturar da forma que foi solicitado no tempo que me foi dado por isso" +
+                "estou enviando o git conforme esta no momento e vou atualizando conforme for possivel " +
+                "https://github.com/sidimarcoelho/fescTest");
+        driver.findElement(By.xpath("//div[@role=\"button\"]//span[*=\"Enviar\"]" +
+                "")).click();
+        Assert.assertEquals("Sua resposta foi registrada.",driver.findElement(By.xpath("//div" +
+                "[@class=\"freebirdFormviewerViewResponseConfirmationMessage\"]")).getText());
     }
 
 
